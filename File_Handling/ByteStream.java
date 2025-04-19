e978c408aa810471131ae0ce8a3c8bfde45c9ec2
Aditya Tile <adityatile99@gmail.com>
2025-04-19 22:06:24 +0530

    learn about byte streams

---
+++ File_Handling/ByteStream.java
@@ -0,0 +1,21 @@
+package File_Handling;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteStream {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("photo.jpg");
+                FileOutputStream out = new FileOutputStream("photo_copy.jpg")) {
+
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
